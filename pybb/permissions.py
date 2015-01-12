942536b1dd51da318165ffb203288fe744bb738f
jswv9@mst.edu <jswv9@mst.edu>
2015-01-12 17:54:56 -0600

    Add parens....

--- pybb/permissions.py
+++ pybb/permissions.py
@@ -52,7 +52,7 @@ class DefaultPermissionHandler(object):
         
     def may_view_category(self, user, category):
         """ return True if `user` may view this category, False if not """
-        if not user.is_authenticated:
+        if not user.is_authenticated():
             if category.group != "Applicant":
                 return False
         viewable_groups = get_viewable_groups(user)
@@ -74,7 +74,7 @@ class DefaultPermissionHandler(object):
         
     def may_view_forum(self, user, forum):
         """ return True if user may view this forum, False if not """
-        if not user.is_authenticated:
+        if not user.is_authenticated():
             if forum.group != "Applicant":
                                 return False
         viewable_groups = get_viewable_groups(user)
@@ -100,7 +100,7 @@ class DefaultPermissionHandler(object):
         
     def may_view_topic(self, user, topic):
         """ return True if user may view this topic, False otherwise """
-        if not user.is_authenticated:
+        if not user.is_authenticated():
             if topic.group != "Applicant":
                 return False
         viewable_groups = get_viewable_groups(user)
@@ -166,7 +166,7 @@ class DefaultPermissionHandler(object):
 
     def may_view_post(self, user, post):
         """ return True if `user` may view `post`, False otherwise """
-        if not user.is_authenticated:
+        if not user.is_authenticated():
             if post.group != "Applicant":
                 return False
         viewable_groups = get_viewable_groups(user)
