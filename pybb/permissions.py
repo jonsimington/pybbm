04617c1c22edca95338e6fe3259cd0cff2af2987
jswv9@mst.edu <jswv9@mst.edu>
2015-01-12 18:12:07 -0600

    CHange view permissions again

--- pybb/permissions.py
+++ pybb/permissions.py
@@ -52,7 +52,7 @@ class DefaultPermissionHandler(object):
         
     def may_view_category(self, user, category):
         """ return True if `user` may view this category, False if not """
-        if not user.is_authenticated():
+        if not user.is_authenticated() and not category.group == "Applicant":
             return False
         viewable_groups = get_viewable_groups(user)
         if category.group in viewable_groups:
@@ -73,9 +73,8 @@ class DefaultPermissionHandler(object):
         
     def may_view_forum(self, user, forum):
         """ return True if user may view this forum, False if not """
-        if not user.is_authenticated():
-            if not forum.group == 'Applicant':
-                return False
+        if not user.is_authenticated() and not forum.group == "Applicant":
+            return False
         viewable_groups = get_viewable_groups(user)
         if forum.group in viewable_groups:
             return True
@@ -99,7 +98,7 @@ class DefaultPermissionHandler(object):
         
     def may_view_topic(self, user, topic):
         """ return True if user may view this topic, False otherwise """
-        if not user.is_authenticated():
+        if not user.is_authenticated() and not topic.group == "Applicant":
             return False
         viewable_groups = get_viewable_groups(user)
         if topic.group in viewable_groups:
@@ -164,7 +163,7 @@ class DefaultPermissionHandler(object):
 
     def may_view_post(self, user, post):
         """ return True if `user` may view `post`, False otherwise """
-        if not user.is_authenticated():
+        if not user.is_authenticated() and not post.group == "Applicant":
             return False
         viewable_groups = get_viewable_groups(user)
         if post.group in viewable_groups:
